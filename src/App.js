f1cdc5c120c882a3cbab969acfb0a08ecd4346ac
Dinc Ozdemir <dincozdemir@gmail.com>
2019-03-30 23:16:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { View, Text } from './components';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     );
   }
 }
 
-export default App;
+const styles = {
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
---
+++ src/components/View/index.web.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Text = (props) => <div {...props} />
+
+export default Text;
